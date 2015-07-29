e8afc240b3ebfd0c7d714372b09dfb83d024694b
Smartwatch Project <smartwatchproject@dartmouths-mini.wireless.dartmouth.edu>
2015-07-28 23:41:21 -0400

    UI improvements on first 2 screens

--- DEN_watch/DEN_watch-Bridging-Header.h
+++ DEN_watch/DEN_watch-Bridging-Header.h
@@ -2,3 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+#ifndef DEN_watch_Bridging_Header_h
+#define DEN_watch_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
---
+++ DEN_watch/FirstCustomSegueUnwind.swift
@@ -0,0 +1,35 @@
+//
+//  FirstCustomSegueUnwind.swift
+//  DEN_watch
+//
+//  Created by Smartwatch Project on 7/28/15.
+//  Copyright © 2015 David Bain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FirstCustomSegueUnwind: UIStoryboardSegue {
+
+    //unwind segue
+    override func perform() {
+        // Assign the source and destination views to local variables.
+        let secondVCView = self.sourceViewController.view as UIView!
+        let firstVCView = self.destinationViewController.view as UIView!
+        
+        let screenHeight = UIScreen.mainScreen().bounds.size.height
+        
+        let window = UIApplication.sharedApplication().keyWindow
+        window?.insertSubview(firstVCView, aboveSubview: secondVCView)
+        
+        // Animate the transition.
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            firstVCView.frame = CGRectOffset(firstVCView.frame, 0.0, screenHeight)
+            secondVCView.frame = CGRectOffset(secondVCView.frame, 0.0, screenHeight)
+            
+            }) { (Finished) -> Void in
+                
+                self.sourceViewController.dismissViewControllerAnimated(false, completion: nil)
+        }
+    }
+}
