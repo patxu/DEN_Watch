742ed8a59c21ab93204b5f5a72296a9a26700674
db2882 <david@dnbain.com>
2015-07-28 22:46:00 -0400

    pull down to refresh working

--- DEN_watch/UserListVC.swift
+++ DEN_watch/UserListVC.swift
@@ -12,56 +12,60 @@ import Parse
 
 class UserListVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-   
+    
     @IBOutlet weak var userTable: UITableView!
     let textCellIdentifier = "TextCell"
     var userArray:NSMutableArray = []
-   
+    
     
     let swiftBlogs = ["Ray Wenderlich", "NSHipster", "iOS Developer Tips", "Jameson Quave", "Natasha The Robot", "Coding Explorer", "That Thing In Swift", "Andrew Bancroft", "iAchieved.it", "Airspeed Velocity"]
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        var query = PFUser.query()!
-        //query.whereKey("username", equalTo:"foo")
-        var girls = query.findObjects()!
-        for value in girls{
-            self.userArray.addObject(value)
-        }
+        loadParseData()
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refresh:", forControlEvents: .ValueChanged)
+        userTable.addSubview(refreshControl)
         userTable.delegate = self
         userTable.dataSource = self
         
     }
-    /*
+    
+    
     func loadParseData(){
-        var query : PFQuery = PFUser.query()
+        let query = PFUser.query()!
+        //query.whereKey("username", equalTo:"foo")
         query.findObjectsInBackgroundWithBlock {
-            (objects:[AnyObject]!, error:NSError!) -> Void in
-            if error == nil {
-                if let objects = objects {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil{
+                if let objects = objects as? [PFObject] {
                     for object in objects {
                         self.userArray.addObject(object)
                     }
                 }
-                self.tableView.reloadData()
+                self.userTable.reloadData()
             } else {
-                println("There was an error")
+                // Log details of the failure
+                print ("error")
             }
         }
     }
-    */
-
+    
+    func refresh(refreshControl: UIRefreshControl) {
+        self.userTable.reloadData()
+        refreshControl.endRefreshing()
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userArray.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
-        
         let row = indexPath.row
-        var user = userArray[row] as! PFUser
-        var username = user["FullName"] as! String!
+        let user = userArray[row] as! PFUser
+        let username = user["FullName"] as! String!
         cell.textLabel?.text = username
         
         return cell
